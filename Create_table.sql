bafa414fac699d575e7539ef8f08ac3d758c511d
roprep <110313142+roprep@users.noreply.github.com>
2024-05-28 21:07:24 -0300

    Add files via upload

---
+++ Create_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE departments (
+	department_id INT NOT Null,
+	department_name VARCHAR (45) NOT NULL,
+	PRIMARY KEY (department_id)
+);
---
+++ INSERT.sql
@@ -0,0 +1,5 @@
+INSERT INTO departments VALUES(5,'Deposito');
+INSERT INTO departments VALUES(6,'Contaduria');
+INSERT INTO departments VALUES(7,'Contratos');
+
+
